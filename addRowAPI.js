5517159b65feb1e61cf2e46704ba3543a1ed2274
Apon Palanuwech <apon@5lab.co>
2021-07-25 16:44:45 +0700

    update

---
+++ addRowAPI.js
@@ -0,0 +1,31 @@
+//
+const { GoogleSpreadsheet } = require('google-spreadsheet')
+
+module.exports = (req, res, next) => {
+  const sheetId = req.body.sheetId
+  const creds = req.body.creds
+  const doc = new GoogleSpreadsheet(sheetId)
+  await doc.useServiceAccountAuth(creds)
+  await doc.loadInfo()
+  const sheet = doc.sheetsByIndex[2]
+  /**
+   * [
+    {
+      firstname: req.body.firstName,
+      lastname: req.body.lastName,
+      organization: req.body.organization,
+      title: req.body.title,
+      email: req.body.email,
+      hear_from: req.body.hear_from,
+      note: req.body.note,
+    },
+  ]
+   */
+  const resAddRow = await sheet.addRows([req.body.fields])
+  // Send email
+  return {
+    success: true,
+    fields: req.body.fields,
+  }
+  //
+}
--- app.js
+++ app.js
@@ -1,32 +1,35 @@
-var api = require('./api');
+const api = require('./api')
+const addRowAPI = require('./addRowAPI')
 
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
 
-var app = express();
+const port = process.env.PORT || 5000
 
-var port = process.env.PORT || 5000;
-
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
 
 // body parser middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 // get api
-app.get('/api', api);
+app.get('/api', api)
+app.get('/v1/api/get', api)
+app.get('/v1/api/add-row', addRowAPI)
 
 // error handler
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.status(400).send(err.message);
-});
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(400).send(err.message)
+})
 
-app.listen(port, function() {
-  console.log('GSX2JSON listening on port ' + port);
-});
+app.listen(port, function () {
+  console.log('GSX2JSON listening on port ' + port)
+})
